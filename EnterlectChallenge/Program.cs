46f9f41d1c2941fde7ff2d491bdf415b1b3a9754
Matthew <crock.matt@gmail.com>
2014-07-02 23:05:20 +0200

    working on making the move choice a little smarter, a little.

--- EnterlectChallenge/Program.cs
+++ EnterlectChallenge/Program.cs
@@ -19,35 +19,35 @@ namespace PacManDuelBot
         private const char ONE_POINTER = '.';
         private const char TEN_POINTER = '*';
         private const char POISON_PILL = '!';
+        private const char EMPTY = ' ';
         private const char PLAYER_A = 'A';
         private const char PLAYER_B = 'B';
         private const String OUTPUT_FILE_NAME = "game.state";
         private const String INITIAL_FILE_NAME = @"..\..\initial.state";
+        private static int runningTally = 0;
 
         static void Main(string[] args)
         {
-            var maze = ReadMaze(INITIAL_FILE_NAME);
-            var coordinate = GetCurrentPosition(maze);
+            var board = ReadMaze(INITIAL_FILE_NAME);
+            var coordinate = GetCurrentPosition(board);
             if (!coordinate.IsEmpty)
             {
-                var possibleMoveList = DetermineMoves(coordinate, maze);
-                var random = new Random();
-                var randomMoveIndex = random.Next(0, possibleMoveList.Count);
-                maze = MakeMove(coordinate, possibleMoveList[randomMoveIndex], maze);
-                WriteMaze(maze, OUTPUT_FILE_NAME);
+                var possibleMoveList = DetermineMoves(coordinate, board);
+                var bestMove = DetermineBestMove(possibleMoveList, coordinate, board);
+                board = MakeMove(coordinate, bestMove, board);
+                WriteMaze(board, OUTPUT_FILE_NAME);
             }
             while (true)
             {
-                maze = ReadMaze(OUTPUT_FILE_NAME);
-                coordinate = GetCurrentPosition(maze);
+                board = ReadMaze(OUTPUT_FILE_NAME);
+                coordinate = GetCurrentPosition(board);
                 if (!coordinate.IsEmpty)
                 {
-                    var possibleMoveList = DetermineMoves(coordinate, maze);
-                    var random = new Random();
-                    var randomMoveIndex = random.Next(0, possibleMoveList.Count);
-                    maze = MakeMove(coordinate, possibleMoveList[randomMoveIndex], maze);
-                    WriteMaze(maze, OUTPUT_FILE_NAME);
-                    Thread.Sleep(1000);
+                    var possibleMoveList = DetermineMoves(coordinate, board);
+                    var bestMove = DetermineBestMove(possibleMoveList, coordinate, board);
+                    board = MakeMove(coordinate, bestMove, board);
+                    WriteMaze(board, OUTPUT_FILE_NAME);
+                    Thread.Sleep(100);
                 }
             }
         }
@@ -69,29 +69,49 @@ namespace PacManDuelBot
             return coordinate;
         }
 
-        private static List<Point> DetermineMoves(Point currentPoint, char[][] maze)
+        public static Point DetermineBestMove(List<Point> possibleMoves ,Point currentPosition, char[][] board)
+        {
+            var random = new Random();
+            var randomMoveIndex = random.Next(0, possibleMoves.Count);
+            var bestMoveSoFar = possibleMoves[0];
+            foreach (var move in possibleMoves)
+            {
+                if (board[move.X][move.Y] != board[bestMoveSoFar.X][bestMoveSoFar.Y])
+                {
+                    if (board[move.X][move.Y] == TEN_POINTER)
+                        bestMoveSoFar = move;
+                    else if (board[move.X][move.Y] == ONE_POINTER)
+                        bestMoveSoFar = move;
+                    //else if (board[move.X][move.Y] == EMPTY)
+                    //    bestMoveSoFar = move;
+                }
+            }
+            return bestMoveSoFar;
+        }
+
+        private static List<Point> DetermineMoves(Point currentPosition, char[][] board)
         {
             var moveList = new List<Point>();
-            if (currentPoint.Y + 1 < WIDTH)
-                if (!maze[currentPoint.X][currentPoint.Y + 1].Equals(WALL))
-                    moveList.Add(new Point { X = currentPoint.X, Y = currentPoint.Y + 1 });
+            if (currentPosition.Y + 1 < WIDTH)
+                if (!board[currentPosition.X][currentPosition.Y + 1].Equals(WALL))
+                    moveList.Add(new Point { X = currentPosition.X, Y = currentPosition.Y + 1 });
 
-            if (currentPoint.Y - 1 >= 0)
-                if (!maze[currentPoint.X][currentPoint.Y - 1].Equals(WALL))
-                    moveList.Add(new Point { X = currentPoint.X, Y = currentPoint.Y - 1 });
+            if (currentPosition.Y - 1 >= 0)
+                if (!board[currentPosition.X][currentPosition.Y - 1].Equals(WALL))
+                    moveList.Add(new Point { X = currentPosition.X, Y = currentPosition.Y - 1 });
 
-            if (currentPoint.X + 1 < HEIGHT)
-                if (!maze[currentPoint.X + 1][currentPoint.Y].Equals(WALL))
-                    moveList.Add(new Point { X = currentPoint.X + 1, Y = currentPoint.Y });
+            if (currentPosition.X + 1 < HEIGHT)
+                if (!board[currentPosition.X + 1][currentPosition.Y].Equals(WALL))
+                    moveList.Add(new Point { X = currentPosition.X + 1, Y = currentPosition.Y });
 
-            if (currentPoint.X - 1 >= 0)
-                if (!maze[currentPoint.X - 1][currentPoint.Y].Equals(WALL))
-                    moveList.Add(new Point { X = currentPoint.X - 1, Y = currentPoint.Y });
+            if (currentPosition.X - 1 >= 0)
+                if (!board[currentPosition.X - 1][currentPosition.Y].Equals(WALL))
+                    moveList.Add(new Point { X = currentPosition.X - 1, Y = currentPosition.Y });
 
-            if (currentPoint.X.Equals(PORTAL1_X) && currentPoint.Y.Equals(PORTAL1_Y))
+            if (currentPosition.X.Equals(PORTAL1_X) && currentPosition.Y.Equals(PORTAL1_Y))
                 moveList.Add(new Point { X = PORTAL2_X, Y = PORTAL2_Y });
 
-            if (currentPoint.X.Equals(PORTAL2_X) && currentPoint.Y.Equals(PORTAL2_Y))
+            if (currentPosition.X.Equals(PORTAL2_X) && currentPosition.Y.Equals(PORTAL2_Y))
                 moveList.Add(new Point { X = PORTAL1_X, Y = PORTAL1_Y });
 
             return moveList;
@@ -99,6 +119,15 @@ namespace PacManDuelBot
 
         private static char[][] MakeMove(Point currentPoint, Point movePoint, char[][] maze)
         {
+            switch (maze[movePoint.X][movePoint.Y])
+            {
+                case ONE_POINTER:
+                    runningTally += 1;
+                    break;
+                case TEN_POINTER:
+                    runningTally += 10;
+                    break;
+            }
             maze[currentPoint.X][currentPoint.Y] = ' ';
             maze[movePoint.X][movePoint.Y] = PLAYER_A;
             return maze;
@@ -125,13 +154,11 @@ namespace PacManDuelBot
 
         private static void PrintToScreen(string outputGameContents)
         {
-            var output = outputGameContents.Replace("#", " #");
-            output = output.Replace("A", " A");
-            output = output.Replace("B", " B");
-            output = output.Replace(".", " .");
-            output = output.Replace("*", " *");
-            output += "\r\n\r\n";
-            Console.Write(output);
+            //in here need to do a nice gui
+            outputGameContents += "\r\n\r\n";
+            Console.Write(outputGameContents);
+            Console.Write(String.Format("Current Tally: {0} \r\n", runningTally));
+
         }
 
         private static TreeNode ReadMazeTree(String filePath)
