971efab242fbb601d3e79b33f9cf127ec4174d8b
Matthew Crockett <crock.matt@gmail.com>
2014-07-02 07:29:18 +0200

    Initial commit.

---
+++ EnterlectChallenge/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnterlectChallenge
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsEmpty { get; set; }
+
+        public Point()
+        {
+
+        }
+    }
+}
---
+++ EnterlectChallenge/Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterlectChallenge
+{
+    class Tree
+    {
+        private Point coOrd;
+        private Tree up, down, left, right;
+
+        public Tree(Point coOrd, Tree left, Tree right)
+        {
+            this.coOrd = coOrd;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Tree(){}
+
+        public Tree(string fileContents)
+        {
+            fileContents.Split('\n');
+        }
+    }
+}
