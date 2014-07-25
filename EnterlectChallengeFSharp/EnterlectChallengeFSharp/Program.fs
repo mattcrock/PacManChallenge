ca4114bd2dec1953bdb825adc1f9e77b9d3ad4a3
Matthew Crockett <crock.matt@gmail.com>
2014-07-25 08:49:38 +0200

    Adding the f#project, for real this time.

---
+++ EnterlectChallengeFSharp/EnterlectChallengeFSharp/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    
+    let readLines filePath = System.IO.File.ReadLines(filePath)
+    
+    //readLines(argv);
+    
+    0 // return an integer exit code
+
